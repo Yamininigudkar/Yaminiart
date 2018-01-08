ea5c154a067c373928fb72ca7fc07d1db30a8bb2
Yamini <yaminipuranik@gmail.com>
2018-01-08 17:35:47 -0600

    models and routes folder added

---
+++ controllers/routecontroller.js
@@ -0,0 +1,98 @@
+let express = require('express');
+let router = new express.Router();
+let path = require('path');
+var session = require('express-session');
+let User = require('../models/user.js');
+let bcrypt = require('bcrypt');
+let Notch = require('../models/notch.js');
+let fs = require('fs');
+var multer = require('multer');
+
+//Routes
+router.get('/', function (req, res) {
+  res.send(path.join(__dirname + './public/index.html'));
+})
+
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now()+'JPG')
+  }
+})
+var upload = multer({ storage: storage });
+
+router.post('/uploadpicturre', function (req, res) {
+  upload(req, res, function (err) {
+    if (err) {
+      return
+    }
+    res.json("success")
+    
+  })
+})
+router.post('/newproduct',upload.any() ,function (req, res) {
+  var imgPath = req.files[0].path
+
+  let newproduct = new Product ({
+    title:req.body.title,
+    category:req.body.category,
+    userId:req.session.userId,
+    description:req.body.description,
+    latitude:req.body.latitude,
+    longitude:req.body.longitude,
+
+  })
+  notchObj.img.data= fs.readFileSync(imgPath);
+  notchObj.img.contentType='image/JPG'
+
+  notchObj.save(function (err, notchObj) {
+    if (err) throw err;
+
+  });
+
+});
+
+
+// Receives and authenticates login information from existing users
+router.post('/existinguser', function(req,res){
+  User.findOne({ 'username': req.body.username }, function (err, user) {
+    if (err) {
+      console.log(err);
+      res.send('unsuccessful');
+    } else if (user == null) {
+      res.send('unsuccessful');
+    } else {
+      var savedHash = user.password;
+      bcrypt.compare(req.body.password, savedHash, function (err, status) {
+                    // console.log(status);
+                    if(status === true){
+                      req.session.userId = user._id
+                      res.json(user)
+                    } else{
+                      res.json('unsuccessful');
+                    } 
+                  });
+    }
+  })
+}
+
+);
+router.get('/logout',function(req,res){
+  res.clearCookie('user_sid')
+  res.json("success")
+})
+
+// Accepts login information from new users, checks if the username exists, and saves the user if unique
+router.post('/newuser', actions.newUser);
+router.post('/getNotches', actions.getNotches);
+router.get('/notches', actions.getAllNotches);
+router.get('/findone/:id', actions.findOne);
+router.get('/findoneimage/:id', actions.findOneImage);
+router.get('/userNotches', actions.userNotches);
+router.post('/deleteNotch', actions.deleteNotch);
+router.get('/notches/list_by_category/:category', actions.searchNotches);
+
+module.exports = router;
